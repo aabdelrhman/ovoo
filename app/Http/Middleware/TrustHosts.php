4dfdc8310436caa9cf10ebe89cd89dad18c6e0d3
hosametm <hosammetman@gmail.com>
2024-02-25 21:31:50 +0200

    chore(hosts): add FE hosts

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -14,6 +14,15 @@ class TrustHosts extends Middleware
     public function hosts(): array
     {
         return [
+            'localhost',
+            'localhost:3000',
+            'localhost:3001',
+            'localhost:5000',
+            'localhost:5173',
+            'http://localhost:3000',
+            'http://localhost:3001',
+            'http://localhost:5000',
+            'http://localhost:5173',
             $this->allSubdomainsOfApplicationUrl(),
         ];
     }
