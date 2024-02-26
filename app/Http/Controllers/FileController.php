463f727fa8e1d46aebec17fe58d7d513ccf3dab7
aabdelrhman <70432309+aabdelrhman@users.noreply.github.com>
2024-02-26 22:41:34 +0200

    Update FileController.php

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -14,7 +14,7 @@ class FileController extends Controller
     {
         try {
             $url = uploadImage($request->file('file'), 'uploads');
-            return $this->returnSuccessRespose('Success', ['url' => Storage::path($url)], 200);
+            return $this->returnSuccessRespose('Success', ['url' => Storage::url($url)], 200);
         } catch (Exception $e) {
             return $this->returnErrorRespose($e->getMessage(), 500);
         }
