87ac9bbb99012c5880129d062def9ccd4f98c794
aabdelrhman <70432309+aabdelrhman@users.noreply.github.com>
2024-03-13 22:53:23 +0200

    return earn date in medals

--- app/Http/Resources/MedalResource.php
+++ app/Http/Resources/MedalResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -20,6 +21,7 @@ class MedalResource extends JsonResource
             'image' => asset($this->image),
             'points' => $this->points,
             'medal_type' => $this->whenLoaded('medalType', fn () => $this->medalType->name),
+            'earned_at' => Carbon::parse($this->earned_at)->format('d.m.Y'),
             // 'description' => $this->description
         ];
     }
