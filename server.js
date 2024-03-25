e01a8f0a9b663f6eaba0d36b34b2b48f53d6202c
aabdelrhman <70432309+aabdelrhman@users.noreply.github.com>
2024-03-25 16:50:39 +0200

    node server

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
-import express from 'express';
-import axios from 'axios';
-import fs from 'fs';
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
 const app = express();
-const baseUrl = 'https://electro-fix.ramime.com/api/v1';
-const certUrl = '/home/ramime/ssl/certs/www_electro_fix_ramime_com_a2b88_5ef4d_1693819388_7933daa92cc437c8253f0e5ef10c126e.crt';
-const CertKey = '/home/ramime/ssl/keys/a2b88_5ef4d_1798ee102b319e33223b01268f530ada.key';
-var privateKey  = fs.readFileSync(CertKey, 'utf8');
+
+// Load SSL certificates
+const certUrl = '/home/admin1/ssl/certs/ovoo_app_c206e_905e5_1716249599_508a752e2b9b96c84ae16b3ce4f8cf8e.crt';
+const certKey = '/home/admin1/ssl/keys/c206e_905e5_88ab613d74c96fa03e27608e42333ad6.key';
+var privateKey  = fs.readFileSync(certKey, 'utf8');
 var certificate = fs.readFileSync(certUrl, 'utf8');
 var credentials = {
     key: privateKey,
     cert: certificate
 };
-var server = require('http').createServer(app);
+
+var server = require('https').createServer(credentials , app);
 const io = require('socket.io')(server, {
     cors: { origin: "*"}
 });
- const sessionsMap = {};
+
+// Define Socket.io event handlers
 io.on('connection', (socket) => {
+    console.log('A user connected');
 
-
-    socket.on('test_test', () => {
-        socket.emit('updateOfferStatus', response.data);
-        socket.broadcast.emit('updateOfferStatus', response.data);
-
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // Broadcast the message to all connected clients
     });
-    socket.on('disconnect', (socket) => {
-        console.log(socket);
-        console.log('Disconnect');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running');
+const port = 6001;
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
