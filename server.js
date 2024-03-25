511849a7b8e3031a9af5e7aee0b2e30458940131
aabdelrhman <70432309+aabdelrhman@users.noreply.github.com>
2024-03-26 00:30:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const app = express();
 
 // Load SSL certificates
+const baseUrl = "http://ovoo.app/api/v1"
 const certUrl = '/home/admin1/ssl/certs/ovoo_app_c206e_905e5_1716249599_508a752e2b9b96c84ae16b3ce4f8cf8e.crt';
 const certKey = '/home/admin1/ssl/keys/c206e_905e5_88ab613d74c96fa03e27608e42333ad6.key';
 var privateKey  = fs.readFileSync(certKey, 'utf8');
@@ -20,14 +21,30 @@ const io = require('socket.io')(server, {
 
 // Define Socket.io event handlers
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    socket.join('chat-'+socket.handshake.query.chat_id);
+    console.log('chat-'+socket.handshake.query.chat_id);
+    socket.on('chat-'+socket.handshake.query.chat_id, (message) => {
+        var token = socket.handshake.headers.authorization;
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": token,
+        };
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg); // Broadcast the message to all connected clients
-    });
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
+        if(token !== undefined){
+            axios.post(baseUrl+'/send-message' , message , {headers})
+                .then(response => {
+                    console.log(response.data);
+                    socket.emit('sendChatToClient', response.data);
+                    socket.to('chat-'+socket.handshake.query.chat_id).emit('sendChatToClient', response.data);
+                })
+                .catch(error => {
+                    console.log(error.response.data);
+                });
+        }else{
+            console.log('Token is required')
+        }
+
     });
 });
 
