dc18b6af52699b1c955b09999cb70e33cfe5d7bd
aabdelrhman <70432309+aabdelrhman@users.noreply.github.com>
2024-03-28 12:01:59 +0200

    Update helper.php

--- app/Helper/helper.php
+++ app/Helper/helper.php
@@ -23,7 +23,8 @@ if (!function_exists('image_resize_save')) {
 if (!function_exists('generateSmsCode')) {
     function generateSmsCode()
     {
-        return rand(10000, 99999);
+        return 00000;
+        // return rand(10000, 99999);
     }
 }
 
